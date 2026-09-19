f3850a8297b8570d4df2416c812f5c80e8029034
Saidu Hassan Nuhu <saidu@example.com>
2026-09-19 22:52:14 +0100

    Set CESI version to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cesi.assistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 }
 
